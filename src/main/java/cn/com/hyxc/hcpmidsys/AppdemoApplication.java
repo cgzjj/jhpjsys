b94c2d67744af97c38b24250fc7609aba677034c
99598468 <l13904589893@163.com>
2020-11-12 11:47:37 +0800

    处理请求

--- src/main/java/cn/com/hyxc/hcpmidsys/AppdemoApplication.java
+++ src/main/java/cn/com/hyxc/hcpmidsys/AppdemoApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("cn.com.hyxc.hcpmidsys.*")
 
+@ComponentScan("cn.com.hyxc.hcpmidsys.*")
 public class AppdemoApplication {
 
     public static void main(String[] args) {
